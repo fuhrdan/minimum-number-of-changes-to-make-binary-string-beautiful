9cf883584ef4397919cca459fc61bd0991a3f1d9
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-11-05 07:03:47 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//*****************************************************************************
+//** 2914. Minimum Number of Changes to Make Binary String Beautiful         **
+//*****************************************************************************
+
+
+int minChanges(char* s) {
+    int len = strlen(s);
+    int count = 0;
+    for(int i = 1; i < len; i += 2)
+    {
+        count += s[i] != s[i-1];
+    }
+    return count;
+}
